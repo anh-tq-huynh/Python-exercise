db2ace419495c3333613e139f698f4acc39ae213
anhhuynh <anhhuy@metropolia.fi>
2024-08-26 12:26:42 +0300

    Updated to module 2

---
+++ Module 2/Task 5.py
@@ -0,0 +1,9 @@
+talent = float(input("Enter the number of talents: "))
+pound  = float(input("Enter the number of pounds: "))
+lot    = float(input("Enter the number of lots: "))
+
+total_lot = (talent * 20 + pound)* 32
+gram = total_lot * 13.3
+
+kilogram = gram // 1000
+print(f'The weight in modern unit is: {kilogram:.0f} kilograms and {gram - (kilogram * 1000):.2f}')
