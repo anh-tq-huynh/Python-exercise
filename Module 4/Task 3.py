88f6c51b67048f97749071af51846e1739630105
anhhuynh <anhhuy@metropolia.fi>
2024-08-29 09:59:12 +0300

    Updated to module 4 - tsk 4

--- Module 4/Task 3.py
+++ Module 4/Task 3.py
@@ -1,11 +1,12 @@
-i = int(input('Enter a number'))
+i = (input('Enter a number'))
 s = i
 l = i
 while i != "":
+    number = float (i)
     if i < s:
         s = i
     if i > l:
         l = i
-    i = int(input('Enter a number'))
+    i = input('Enter a number')
 else:
-print(f'The largest number is: {l}, and the smallest number is {s}')
+    print(f'The largest number is: {l}, and the smallest number is {s}')
